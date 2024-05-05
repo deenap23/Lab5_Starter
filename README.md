3ff1b30c12f85bfedd861997aac87d91c0b83ae9
deenap23 <142956234+deenap23@users.noreply.github.com>
2024-05-05 16:30:36 -0700

    add unit tests

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Lab 5 - Starter
 Deena Pederson
+A16309065
+
+View: [Expose.html](https://deenap23.github.io/Lab5_Starter/expose.html)
+
+View: [Explore.html](https://deenap23.github.io/Lab5_Starter/explore.html)
+
+### Check your understanding
+1. In this scenario, unit testing would not be the most effective way to test. The message feature involves multiple components interacting with each other, and this is something that can not be tested using unit testing on the application/feature level.
+2. On the other hand, this is a case that would be able to be properly tested using unit testing. This is a case where you could quickly execute tests and make sure the application runs as expected for different character lengths. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,68 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+// isPhoneNumber
+test('7 digit phone number', () => {
+  expect(isPhoneNumber("408-4421")).toBe(true);
+});
+test('10 digit phone number', () => {
+  expect(isPhoneNumber("408-442-1234")).toBe(true);
+});
+test('3 digit invalid phone number', () => {
+  expect(isPhoneNumber("408")).toBe(false);
+});
+test('improper format invalid phone number', () => {
+  expect(isPhoneNumber("4084416142")).toBe(false);
+});
+// isEmail
+test('short valid email', () => {
+  expect(isEmail("deena@gmail.com")).toBe(true);
+});
+test('long complicated valid email', () => {
+  expect(isEmail("deenapedersonilovecoding123@gmail.com")).toBe(true);
+});
+test('improper email format', () => {
+  expect(isEmail("deena@gmail")).toBe(false);
+});
+test('improper email format 2', () => {
+  expect(isEmail("deena.gmail.com")).toBe(false);
+});
+// isStrongPassword
+test('long letters only password', () => {
+  expect(isStrongPassword("deenapederson")).toBe(true);
+});
+test('letters only email', () => {
+  expect(isStrongPassword("ilikedogs_23")).toBe(true);
+});
+test('too short email invalid', () => {
+  expect(isStrongPassword("red")).toBe(false);
+});
+test('non letter first character', () => {
+  expect(isStrongPassword("1_hate_red")).toBe(false);
+});
+// isDate
+test('test valid date with single char day/month', () => {
+  expect(isDate("1/1/2011")).toBe(true);
+});
+test('test valid date with double char day/month', () => {
+  expect(isDate("10/10/2011")).toBe(true);
+});
+test('test invalid date with double char year', () => {
+  expect(isDate("10/10/24")).toBe(false);
+});
+test('test invalid date format', () => {
+  expect(isDate("10-10-2011")).toBe(false);
+});
+// isHexColor
+test('6 char hex color', () => {
+  expect(isHexColor("#2f2f11")).toBe(true);
+});
+test('3 char hex color', () => {
+  expect(isHexColor("333")).toBe(true);
+});
+test('invalid 8 char hex code', () => {
+  expect(isHexColor("#22221111")).toBe(false);
+});
+test('wrong format invalid character', () => {
+  expect(isHexColor("-222211")).toBe(false);
+});
